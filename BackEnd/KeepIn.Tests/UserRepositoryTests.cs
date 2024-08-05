821fba944b454a814317657337f08951469cf361
rorychatt <pavel.rinne@gmail.com>
2024-08-05 18:20:07 +0200

    Feat: user repository test setup

---
+++ BackEnd/KeepIn.Tests/UserRepositoryTests.cs
@@ -0,0 +1,14 @@
+﻿using KeepIn.Api.Models;
+
+namespace KeepIn.Tests;
+
+public class UserRepositoryTests
+{
+    [Fact]
+    public void UserRepositoryShould_HaveGetAllUsersMethod()
+    {
+        var userRepository = new UserRepository();
+        
+        Assert.NotNull(userRepository.GetAllUsers());
+    }
+}
