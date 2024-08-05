5b602ea69ced74eac0419b1b71dd44ee3b213151
rorychatt <pavel.rinne@gmail.com>
2024-08-05 18:19:49 +0200

    Feat: users controller tests

---
+++ BackEnd/KeepIn.Tests/UsersControllerTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace KeepIn.Tests;
+
+public class UsersControllerTests(WebApplicationFactory<Program> factory)
+    : IClassFixture<WebApplicationFactory<Program>>
+{
+    [Fact]
+    public async Task UsersControllerShould_ReturnListOfUsers()
+    {
+        var client = factory.CreateClient();
+        var response = await client.GetAsync("/users");
+
+        response.EnsureSuccessStatusCode();
+    }
+}
