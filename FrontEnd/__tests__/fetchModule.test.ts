2277b66ae7759571aea30fc4376c00f87d72ebf8
rorychatt <pavel.rinne@gmail.com>
2024-08-09 14:59:31 +0200

    Info: test for fetching method

---
+++ FrontEnd/__tests__/fetchModule.test.ts
@@ -0,0 +1,9 @@
+﻿import {describe, expect, test} from "vitest";
+import {fetchModulesAsync} from "../src/api";
+
+describe("fetchModules", () => {
+    test("should not crash", async () => {
+        const response = await fetchModulesAsync();
+        expect(response).toBeDefined();
+    })
+})
