250016b18711c32dba92baee4900330aaf6657b4
rorychatt <pavel.rinne@gmail.com>
2024-08-09 13:14:30 +0200

    Feat: fixed issue for IModule json deserializer

--- BackEnd/KeepIn.Business/Contracts/IModule.cs
+++ BackEnd/KeepIn.Business/Contracts/IModule.cs
@@ -4,7 +4,7 @@ namespace KeepIn.Business.Contracts;
 
 public interface IModule : IIdentifiable
 {
-    public abstract record Properties()
+    public record Properties()
     {
         public required string Name { get; init; }
         public required string Version { get; init; }
@@ -12,11 +12,16 @@ public interface IModule : IIdentifiable
         public string? Author { get; init; }
         public string? License { get; init; }
         public Dictionary<string, string> Dependencies { get; init; } = new();
+
+        public Properties(string name, string version) : this()
+        {
+            Name = name;
+            Version = version;
+        }
     }
-    
+
     public record BaseModuleConfigJson()
     {
-        [JsonPropertyName("Properties")]
-        public Properties? Properties { get; init; }
+        [JsonPropertyName("Properties")] public Properties? Properties { get; init; }
     }
 }
