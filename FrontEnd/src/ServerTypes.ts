2b9eeef9cbab3bf144be53a1f6d63f7e18958e43
rorychatt <pavel.rinne@gmail.com>
2024-08-09 14:33:20 +0200

    Feat: types for server responses

---
+++ FrontEnd/src/ServerTypes.ts
@@ -0,0 +1,18 @@
+﻿export type Module = {
+    id: string;
+    properties: ModuleProperties;
+}
+    
+export type ModuleProperties = {
+    name: string,
+    version: string,
+    description: string,
+    author: string,
+    license: string,
+    dependencies: Record<string, string>
+}
+
+export type ServerUserResponse = {
+    name: string;
+    modules: Module[];
+}
