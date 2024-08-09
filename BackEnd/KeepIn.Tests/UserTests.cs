d8ae15bc8c2bcaf60639951fde20f813bb2f60cd
rorychatt <pavel.rinne@gmail.com>
2024-08-09 14:04:44 +0200

    Info: get module by name should work

--- BackEnd/KeepIn.Tests/UserTests.cs
+++ BackEnd/KeepIn.Tests/UserTests.cs
@@ -1,6 +1,6 @@
-﻿using KeepIn.Business.Users;
+﻿using FluentAssertions;
+using KeepIn.Business.Users;
 using KeepIn.Modules.InventoryManager;
-using Microsoft.AspNetCore.Identity;
 
 namespace KeepIn.Tests;
 
@@ -10,7 +10,7 @@ public class UserTests
     public void UserShould_HaveUniqueId()
     {
         var user = new User("kENT2231");
-        
+
         Assert.NotEqual("user_", user.Id);
     }
 
@@ -18,7 +18,7 @@ public class UserTests
     public void UserShould_HaveListOfActiveModules()
     {
         var user = new User("Wekt1221");
-        
+
         Assert.NotNull(user.ActiveModules);
     }
 
@@ -26,23 +26,26 @@ public class UserTests
     public void UserShould_RegisterNewModule()
     {
         var user = new User("Peterson");
-        
+
         var moduleCountBefore = user.ActiveModules.Count();
-        
+
         user.RegisterNewModule(new InventoryManager());
-        
+
         var moduleCountAfter = user.ActiveModules.Count();
-        
+
         Assert.Equal(moduleCountBefore + 1, moduleCountAfter);
     }
+
+    [Fact]
+    public void UserShould_HaveGetModuleByName_Working()
+    {
+        var user = new User("Peterson");
     
-    // [Fact]
-    // public void UserShould_HaveCorrect_ModuleName_ForInventoryManager()
-    // {
-    //     var user = new User("Peterson");
-    //     
-    //     var addedModule = user.RegisterNewModule(new InventoryManager());
-    //     
-    //     
-    // }
+        var addedModule = user.RegisterNewModule(new InventoryManager())!;
+    
+        var foundModule = user.GetModuleByName(addedModule.Properties.Name);
+    
+        foundModule.Should().NotBeNull();
+        foundModule.Properties.Name.Should().Be(addedModule.Properties.Name);
+    }
 }
