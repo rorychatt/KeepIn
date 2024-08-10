11e0f43129edd85891f5799f99a50c6638ef7f93
rorychatt <pavel.rinne@gmail.com>
2024-08-10 21:43:33 +0200

    Feat: dynamic table for dependency injection

---
+++ BackEnd/KeepIn.Business/Core/DynamicTable.cs
@@ -0,0 +1,9 @@
+﻿using KeepIn.Business.Contracts;
+
+namespace KeepIn.Business.Core;
+
+public class DynamicTable<T, V>(string tableName, Dictionary<T, V> data) : ITable<T, V>
+{
+    public string TableName { get; init; } = tableName;
+    public Dictionary<T, V> Data { get; init; } = data;
+}
