2849437d591a86a1c483b1aa71c51b5165008f9a
rorychatt <pavel.rinne@gmail.com>
2024-08-10 21:16:07 +0200

    Info: server core prep

---
+++ BackEnd/KeepIn.Business/Core/IKeepInCore.cs
@@ -0,0 +1,12 @@
+﻿using KeepIn.Business.Contracts;
+
+namespace KeepIn.Business.Core;
+
+public interface IKeepInCore
+{
+    public Dictionary<string, IModule> ActivatedModules { get; init; }
+    
+    public void ActivateModule(IModule module);
+    public void DeactivateModule(IModule module);
+    
+}
---
+++ BackEnd/KeepIn.Business/Core/KeepInCore.cs
@@ -0,0 +1,24 @@
+﻿using KeepIn.Business.Contracts;
+
+namespace KeepIn.Business.Core;
+
+public class KeepInCore : IKeepInCore
+{
+    public Dictionary<string, IModule> ActivatedModules { get; init; }
+    
+    public void ActivateModule(IModule module)
+    {
+        if (!ActivatedModules.TryAdd(module.Properties.Name, module))
+        {
+            throw new Exception($"Module {module.Properties.Name} already activated");
+        }
+    }
+
+    public void DeactivateModule(IModule module)
+    {
+        if (!ActivatedModules.Remove(module.Properties.Name))
+        {
+            throw new Exception($"Module {module.Properties.Name} not activated");
+        }
+    }
+}
