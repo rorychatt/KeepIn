b83399c9a28c0c813928a8c9a37a3c173ada5bd8
rorychatt <pavel.rinne@gmail.com>
2024-08-11 20:40:58 +0200

    Feat: employee fluent api

--- BackEnd/KeepIn.Modules/EmployeeManager/Employee.cs
+++ BackEnd/KeepIn.Modules/EmployeeManager/Employee.cs
@@ -10,4 +10,35 @@ public class Employee : IIdentifiable
     public string? PhoneNumber { get; set; }
     public string? Address { get; set; }
     public Role? Role { get; set; } = Modules.EmployeeManager.Role.Guest;
+    
+    public Employee SetName(string name)
+    {
+        Name = name;
+        return this;
+    }
+    
+    public Employee SetEmail(string email)
+    {
+        Email = email;
+        return this;
+    }
+    
+    public Employee SetPhoneNumber(string phoneNumber)
+    {
+        PhoneNumber = phoneNumber;
+        return this;
+    }
+    
+    public Employee SetAddress(string address)
+    {
+        Address = address;
+        return this;
+    }
+    
+    public Employee SetRole(Role role)
+    {
+        Role = role;
+        return this;
+    }
+    
 }
