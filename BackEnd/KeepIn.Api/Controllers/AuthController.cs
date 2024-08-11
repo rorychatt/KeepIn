9bd466ca8108e98862f3e0672998743acef4e8ea
rorychatt <pavel.rinne@gmail.com>
2024-08-11 21:04:26 +0200

    Feat: auth controller login logic

---
+++ BackEnd/KeepIn.Api/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using KeepIn.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KeepIn.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    [HttpPost("login")]
+    public IActionResult Login([FromBody] LoginRequest request)
+    {
+        if (request is { Username: "admin", Password: "password" })
+        {
+            return Ok();
+        }
+
+        return Unauthorized();
+    }
+}
