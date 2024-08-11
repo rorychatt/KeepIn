aab87fafc35ac465451e9350f3ad3bf4be942220
rorychatt <pavel.rinne@gmail.com>
2024-08-11 20:43:21 +0200

    Feat: add employee method

--- BackEnd/KeepIn.Modules/EmployeeManager/EmployeeManager.cs
+++ BackEnd/KeepIn.Modules/EmployeeManager/EmployeeManager.cs
@@ -4,5 +4,25 @@ namespace KeepIn.Modules.EmployeeManager;
 
 public class EmployeeManager : BaseModule
 {
-    public readonly Dictionary<string, Employee> Employees = new();
+    public Dictionary<string, Employee> Employees { get; } = new();
+
+    public bool AddEmployee(Employee employee)
+    {
+        return Employees.TryAdd(employee.Id, employee);
+    }
+
+    // public void RemoveEmployee(string employeeId)
+    // {
+    //     Employees.Remove(employeeId);
+    // }
+    //
+    // public Employee GetEmployee(string employeeId)
+    // {
+    //     return Employees[employeeId];
+    // }
+    //
+    // public void UpdateEmployeeRole(string employeeId, Role role)
+    // {
+    //     Employees[employeeId].SetRole(role);
+    // }
 }
