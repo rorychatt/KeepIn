e3facc092f55fa54935dd8e56100e90a7b9f1588
rorychatt <pavel.rinne@gmail.com>
2024-08-11 21:06:25 +0200

    Feat: setup for auth controller tests

---
+++ BackEnd/KeepIn.Tests/AuthControllerTests.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace KeepIn.Tests;
+
+public class AuthControllerTests(WebApplicationFactory<Program> factory)
+    : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly HttpClient _client = factory.CreateClient();
+    
+    
+}
