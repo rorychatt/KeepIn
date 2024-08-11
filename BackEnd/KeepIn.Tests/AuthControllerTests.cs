89b06e3da709cb3496777e0e3ec1b015df640dfe
rorychatt <pavel.rinne@gmail.com>
2024-08-11 21:08:39 +0200

    Feat: test for auth server stuff

--- BackEnd/KeepIn.Tests/AuthControllerTests.cs
+++ BackEnd/KeepIn.Tests/AuthControllerTests.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using System.Net;
+using System.Text;
+using System.Text.Json;
+using FluentAssertions;
+using KeepIn.Api.Models;
+using Microsoft.AspNetCore.Mvc.Testing;
 
 namespace KeepIn.Tests;
 
@@ -6,6 +11,16 @@ public class AuthControllerTests(WebApplicationFactory<Program> factory)
     : IClassFixture<WebApplicationFactory<Program>>
 {
     private readonly HttpClient _client = factory.CreateClient();
-    
-    
+
+    [Fact]
+    public async Task Should_ReturnOk_When_LoginWithAdmin()
+    {
+        var response = await _client.PostAsync("/api/Auth/login",
+            new StringContent(JsonSerializer.Serialize(
+                    new LoginRequest("admin", "password")),
+                Encoding.UTF8, "application/json"
+            ));
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
 }
