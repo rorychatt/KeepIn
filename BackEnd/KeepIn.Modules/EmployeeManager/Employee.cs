ff100b0b600e626bfe381ba519c34f2d54affb3b
rorychatt <pavel.rinne@gmail.com>
2024-08-11 20:36:54 +0200

    Feat: employee class init

---
+++ BackEnd/KeepIn.Modules/EmployeeManager/Employee.cs
@@ -0,0 +1,13 @@
+﻿using KeepIn.Business.Contracts;
+
+namespace KeepIn.Modules.EmployeeManager;
+
+public class Employee : IIdentifiable
+{
+    public string Id { get; init; } = $"employee_{Guid.NewGuid()}";
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? Address { get; set; }
+    public Role? Role { get; set; } = Modules.EmployeeManager.Role.Guest;
+}
