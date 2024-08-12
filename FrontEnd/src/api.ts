3743b1117c5e213bd9107d6ff394d7097108b8b9
rorychatt <pavel.rinne@gmail.com>
2024-08-12 09:23:03 +0200

    Info: new mock server user response

--- FrontEnd/src/api.ts
+++ FrontEnd/src/api.ts
@@ -1,5 +1,4 @@
-﻿import * as ServerTypes from "./ServerTypes.ts";
-import {ServerUserResponse} from "./ServerTypes.ts";
+﻿import {ServerUserResponse} from "./ServerTypes.ts";
 
 export async function fetchModulesAsync(): Promise<ServerUserResponse> {
     try {
@@ -15,12 +14,12 @@ export async function fetchModulesAsync(): Promise<ServerUserResponse> {
     }
 }
 
-export const MOCK_SERVER_USER_RESPONSE: ServerTypes.ServerUserResponse = {
+export const MOCK_SERVER_USER_RESPONSE: ServerUserResponse = {
     "name": "John Stevenson",
     "modules": [
         {
             "properties": {
-                "name": "InventoryManager",
+                "name": "Inventory Manager",
                 "version": "1.0.0",
                 "description": "Base module for KeepIn",
                 "author": "KeepIn",
@@ -29,11 +28,11 @@ export const MOCK_SERVER_USER_RESPONSE: ServerTypes.ServerUserResponse = {
                     "BaseModule": "1.0.0"
                 }
             },
-            "id": "module_c5d22458-3a6c-4a69-8e42-2451f6da1cd5"
+            "id": "module_9184eafe-335c-457a-a9f3-5b06a2bfc2ec"
         },
         {
             "properties": {
-                "name": "InventoryManager",
+                "name": "Employee Manager",
                 "version": "1.0.0",
                 "description": "Base module for KeepIn",
                 "author": "KeepIn",
@@ -42,137 +41,128 @@ export const MOCK_SERVER_USER_RESPONSE: ServerTypes.ServerUserResponse = {
                     "BaseModule": "1.0.0"
                 }
             },
-            "id": "module_9e870d53-f814-4f97-a8b0-55062fcb83ab"
+            "id": "module_21769838-e3af-4784-a31f-9e9f5b508c55"
         },
         {
             "properties": {
-                "name": "InventoryManager",
+                "name": "BaseModule",
                 "version": "1.0.0",
                 "description": "Base module for KeepIn",
                 "author": "KeepIn",
                 "license": "MIT",
-                "dependencies": {
-                    "BaseModule": "1.0.0"
-                }
+                "dependencies": {}
             },
-            "id": "module_b16f29c0-da4d-47a7-920d-e9e5baad0ff8"
+            "id": "module_ffb172c7-45e5-4d5a-ad15-3fea25d50795"
         },
         {
             "properties": {
-                "name": "InventoryManager",
+                "name": "BaseModule",
                 "version": "1.0.0",
                 "description": "Base module for KeepIn",
                 "author": "KeepIn",
                 "license": "MIT",
-                "dependencies": {
-                    "BaseModule": "1.0.0"
-                }
+                "dependencies": {}
             },
-            "id": "module_f5c255d8-68fd-4b88-843e-ca480496d5e7"
+            "id": "module_0d3eb9f3-7dc4-4652-b5af-3444a395c75d"
         },
         {
             "properties": {
-                "name": "InventoryManager",
+                "name": "BaseModule",
                 "version": "1.0.0",
                 "description": "Base module for KeepIn",
                 "author": "KeepIn",
                 "license": "MIT",
-                "dependencies": {
-                    "BaseModule": "1.0.0"
-                }
+                "dependencies": {}
             },
-            "id": "module_35510eda-34ba-4026-ba3b-87d4ae7f0d88"
+            "id": "module_73fdd1b1-13f7-410b-8141-5c977b8674fa"
         },
         {
             "properties": {
-                "name": "InventoryManager",
+                "name": "BaseModule",
                 "version": "1.0.0",
                 "description": "Base module for KeepIn",
                 "author": "KeepIn",
                 "license": "MIT",
-                "dependencies": {
-                    "BaseModule": "1.0.0"
-                }
+                "dependencies": {}
             },
-            "id": "module_3f999095-40cd-4b61-a2b5-d8e7c6b307be"
+            "id": "module_fc5606a1-ae43-466f-acca-93bf931ccb65"
         },
         {
             "properties": {
-                "name": "InventoryManager",
+                "name": "BaseModule",
                 "version": "1.0.0",
                 "description": "Base module for KeepIn",
                 "author": "KeepIn",
                 "license": "MIT",
-                "dependencies": {
-                    "BaseModule": "1.0.0"
-                }
+                "dependencies": {}
             },
-            "id": "module_259deb35-6b25-4fdd-bcee-64aa52dab86f"
+            "id": "module_013ce262-e48e-4f8b-9366-170433c905ee"
         },
         {
             "properties": {
-                "name": "InventoryManager",
+                "name": "BaseModule",
                 "version": "1.0.0",
                 "description": "Base module for KeepIn",
                 "author": "KeepIn",
                 "license": "MIT",
-                "dependencies": {
-                    "BaseModule": "1.0.0"
-                }
+                "dependencies": {}
             },
-            "id": "module_a8761944-6769-4bd0-a1c1-f722d009c29c"
+            "id": "module_025bc23a-f119-4ac6-a3ae-ef2b8e4185fe"
         },
         {
             "properties": {
-                "name": "InventoryManager",
+                "name": "BaseModule",
                 "version": "1.0.0",
                 "description": "Base module for KeepIn",
                 "author": "KeepIn",
                 "license": "MIT",
-                "dependencies": {
-                    "BaseModule": "1.0.0"
-                }
+                "dependencies": {}
             },
-            "id": "module_e1e1d577-2d21-457b-b960-42837b328d54"
+            "id": "module_99aebaea-345c-4f54-919d-f6c433fa9552"
         },
         {
             "properties": {
-                "name": "InventoryManager",
+                "name": "BaseModule",
                 "version": "1.0.0",
                 "description": "Base module for KeepIn",
                 "author": "KeepIn",
                 "license": "MIT",
-                "dependencies": {
-                    "BaseModule": "1.0.0"
-                }
+                "dependencies": {}
             },
-            "id": "module_f7436cc5-1797-484d-9f59-fe222e420bd8"
+            "id": "module_fc943cca-b7fc-403e-9c8c-c48190efe4fb"
         },
         {
             "properties": {
-                "name": "InventoryManager",
+                "name": "BaseModule",
                 "version": "1.0.0",
                 "description": "Base module for KeepIn",
                 "author": "KeepIn",
                 "license": "MIT",
-                "dependencies": {
-                    "BaseModule": "1.0.0"
-                }
+                "dependencies": {}
             },
-            "id": "module_269dc628-fc3c-4828-83ae-0b57e7e71340"
+            "id": "module_04f857d3-cced-4662-8721-9995438a3f6e"
         },
         {
             "properties": {
-                "name": "InventoryManager",
+                "name": "BaseModule",
                 "version": "1.0.0",
                 "description": "Base module for KeepIn",
                 "author": "KeepIn",
                 "license": "MIT",
-                "dependencies": {
-                    "BaseModule": "1.0.0"
-                }
+                "dependencies": {}
             },
-            "id": "module_08e99595-927b-47f6-9162-0762a70697dc"
+            "id": "module_35719d2e-eca9-4448-b671-dee023aa9f52"
+        },
+        {
+            "properties": {
+                "name": "BaseModule",
+                "version": "1.0.0",
+                "description": "Base module for KeepIn",
+                "author": "KeepIn",
+                "license": "MIT",
+                "dependencies": {}
+            },
+            "id": "module_9dd28cd3-d8d5-4ca7-929e-ce4688d38f23"
         }
     ]
-}
+};
