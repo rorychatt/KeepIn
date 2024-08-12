603eb0e6cb268a89d8e0ebb1d65334f6120b9a17
rorychatt <pavel.rinne@gmail.com>
2024-08-12 10:45:59 +0200

    Feat: inject keepincore

--- BackEnd/KeepIn.Api/Program.cs
+++ BackEnd/KeepIn.Api/Program.cs
@@ -1,4 +1,5 @@
 using KeepIn.Api.Models;
+using KeepIn.Business.Core;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddSingleton<IUserRepository, UserRepository>();
+builder.Services.AddSingleton<IKeepInCore, KeepInCore>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
