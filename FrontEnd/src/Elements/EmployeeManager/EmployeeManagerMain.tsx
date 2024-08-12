2252e5b75c3eb0d170dc48ef90c709231969d7e2
rorychatt <pavel.rinne@gmail.com>
2024-08-12 13:24:30 +0200

    Info: table injection

--- FrontEnd/src/Elements/EmployeeManager/EmployeeManagerMain.tsx
+++ FrontEnd/src/Elements/EmployeeManager/EmployeeManagerMain.tsx
@@ -1,9 +1,34 @@
 ﻿import {Module} from "../../ServerTypes.ts";
 
-const EmployeeManagerMain = ({module}: {module: Module})  =>{
+
+function EmployeesTable() {
+    return <table>
+        <thead>
+        <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Phone Number</th>
+            <th>Address</th>
+            <th>Role</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>The best dude ever</td>
+            <td>test@gmail.com</td>
+            <td>07612311</td>
+            <td>None of your business</td>
+            <td>1</td>
+        </tr>
+        </tbody>
+    </table>
+}
+
+const EmployeeManagerMain = ({module}: { module: Module }) => {
     return <div>
         <h2 className="text-2xl font-bold mb-4">{module.properties.name}</h2>
         <p>{module.properties.description}</p>
+        <EmployeesTable/>
     </div>
 }
 
