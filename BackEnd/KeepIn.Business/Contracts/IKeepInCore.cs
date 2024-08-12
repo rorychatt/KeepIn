fbbded4eaba2ba8d0d16ea295f7daaa561c0fa88
rorychatt <pavel.rinne@gmail.com>
2024-08-12 11:06:41 +0200

    Info: cleanup

--- BackEnd/KeepIn.Business/Contracts/IKeepInCore.cs
+++ BackEnd/KeepIn.Business/Contracts/IKeepInCore.cs
@@ -1,11 +1,8 @@
-﻿using KeepIn.Business.Contracts;
-
-namespace KeepIn.Business.Core;
+﻿namespace KeepIn.Business.Contracts;
 
 public interface IKeepInCore
 {
     public Dictionary<string, IModule> ActivatedModules { get; init; }
-
     public void ActivateModule(IModule module);
     public void DeactivateModule(IModule module);
     public bool TryInjectTable(
