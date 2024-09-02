296ab7ee20563020055c2341c5010e3e505cf1f8
rorychatt <pavel.rinne@gmail.com>
2024-09-02 12:49:57 +0200

    Info: default paths

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
+cd /git/rorychatt/KeepIn
 git pull origin release
 kill -9 $(lsof -t -i:5069)
 dotnet build -c Release
-nohup dotnet run --urls "http://*:5069" > ./logs.txt 2>&1 &
+nohup dotnet run --urls "http://*:5069" > /git/rorychatt/KeepIn/logs.txt 2>&1 &
